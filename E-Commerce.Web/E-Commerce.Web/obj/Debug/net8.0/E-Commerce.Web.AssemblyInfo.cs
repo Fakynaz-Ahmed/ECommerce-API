ea9ffd8205eb140497bdbc92e90a6bdd8b0f051b
Fakynaz-Ahmed <fakyahmed4@gmail.com>
2025-06-28 17:57:51 +0300

    E-Commerce.Web Layer(Delete Empty File)

--- E-Commerce.Web/E-Commerce.Web/obj/Debug/net8.0/E-Commerce.Web.AssemblyInfo.cs
+++ E-Commerce.Web/E-Commerce.Web/obj/Debug/net8.0/E-Commerce.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("E-Commerce.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2058c4d47e731321d92692846a224f0be8ea2e07")]
 [assembly: System.Reflection.AssemblyProductAttribute("E-Commerce.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("E-Commerce.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- E-Commerce.Web/Presentation/obj/Debug/net8.0/Presentation.AssemblyInfo.cs
+++ E-Commerce.Web/Presentation/obj/Debug/net8.0/Presentation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2058c4d47e731321d92692846a224f0be8ea2e07")]
 [assembly: System.Reflection.AssemblyProductAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
